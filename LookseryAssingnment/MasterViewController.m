b36b8b49f5c02d9781c7923902570f68ce8a811a
Valeriy Van <github@w7software.com>
2015-03-25 16:55:33 +0200

    Удалены остатки ненужного кода.

--- LookseryAssingnment/MasterViewController.m
+++ LookseryAssingnment/MasterViewController.m
@@ -20,10 +20,6 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        self.clearsSelectionOnViewWillAppear = NO;
-        self.preferredContentSize = CGSizeMake(320.0, 600.0);
-    }
     database = [PersonsDatabase singleton];
 }
 
