2bd21faeb92d3bb7d6dd69357330c9e4f73865f6
Valeriy Van <val@w7software.com>
2015-03-31 22:08:35 +0300

    Убрал старый мусор.

--- LookseryAssingnment/MasterViewController.m
+++ LookseryAssingnment/MasterViewController.m
@@ -25,12 +25,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    //self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
-    self.detailViewController = (PersonlViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
 }
 
 - (void)didReceiveMemoryWarning {
